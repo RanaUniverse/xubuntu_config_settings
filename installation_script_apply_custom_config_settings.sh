7dc29bc400d2a09903398e71fa86e8bfeb678724
RanaUniverse <RanaUniverse321@gmail.com>
2025-07-26 20:07:12 +0530

    Username related problem will not happens from now this it will change the username in some config files it will be done in the installation script

--- installation_script_apply_custom_config_settings.sh
+++ installation_script_apply_custom_config_settings.sh
@@ -8,6 +8,24 @@ DEST="$HOME/.config/xfce4"
 # Force delete everything inside the destination directory
 rm -rfv "${DEST:?}/"*
 
+
+
+echo "📸 Updating screenshot save path to use the current username in the screenshooter-16.rc file"
+sed -i "s/RANA_UNIVERSE/$(whoami)/g" panel/screenshooter-16.rc
+
+
+echo "🖼️ Setting the desktop wallpaper path with the current username in xfce4-desktop.xml"
+sed -i "s/RANA_UNIVERSE/$(whoami)/g" xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+
+
+echo "🧩 Updating panel configuration to reflect the current username in xfce4-panel.xml" 
+sed -i "s/RANA_UNIVERSE/$(whoami)/g" xfconf/xfce-perchannel-xml/xfce4-panel.xml
+
+
+echo "✅ All username replacements completed successfully!"
+
+
+
 # Copy all files and folders from current working directory to destination
 cp -rv ./* "$DEST/"
 
